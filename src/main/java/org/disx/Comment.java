bcfa21e8f7a7663b0c2535ba450a407d5336c6ff
derenserce <sercederen@gmail.com>
2024-06-12 12:48:24 +0200

    first commit

---
+++ src/main/java/org/disx/Comment.java
@@ -0,0 +1,37 @@
+package org.disx;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.UUID;
+import java.time.LocalDateTime;
+
+// @Data
+// @Entity
+// @Table(name = "comment")
+// @AllArgsConstructor
+// @NoArgsConstructor
+public class Comment {
+
+    // @Id
+    // @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // private Long id;
+
+    // @Column(name = "content", nullable = false, length = 4000)
+    // private String content;
+
+    // @Column(name = "username", nullable = false, length = 280)
+    // private String username;
+
+    // @Column(name = "user_id", nullable = false)
+    // private UUID userId;
+
+    // @Column(name = "created_at", nullable = false)
+    // private LocalDateTime createdAt;
+}
